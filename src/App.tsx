c8b020d40b80aee772fccad61ebaf15e396022a7
Daniela Buzatu <daniela.buzatu@crispico.com>
2023-06-23 15:34:19 +0300

    Add markdown on button

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,26 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Markdown from 'markdown-to-jsx'
 
+// the problem also reproduces on input:
+`
+|a_a_a|<small></small><small></small><small></small><small></small><small></small><small></small><small></small><small></small>
+`
 function App() {
-  const [count, setCount] = useState(0)
+  const [clickedButton, setClickedButton] = useState(false);
 
   return (
     <>
-      <Markdown>{
+      <button onClick={() => {
+          setClickedButton(true); }}>
+            My button
+      </button> 
+      { clickedButton ? 
+          <Markdown>{
 `
 |VRAC_T_1|<small>\`🟩-2\`</small> VRAC|<small>🔵<i>end address of original task</i></small><br /><small>\`🟦-1\`</small> F68|<small>🔵<i>parking of \`AF0374\`</i></small><br /><small>\`🟦-6\`</small> E27|
 `
-      }</Markdown>
+        }</Markdown> 
+         : <></>}
     </>
   )
 }
